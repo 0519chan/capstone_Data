ddafd8d9f00fb2f8d7c9535aec40541535353a1b
chan <laiepang@naver.com>
2025-03-29 20:18:32 +0900

    commit message

--- crawler/app.py
+++ crawler/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
-from crawler import crawl_job_data
+from jobkorea_crwaler import crawl_job_data
+from workent_crawler import crawl_job_data
 
 app = Flask(__name__)
 
@@ -7,12 +8,17 @@ app = Flask(__name__)
 def home():
     return "Hello! This is the Crawler API Server."
 
-@app.route('/api/jobs', methods=['GET'])
-def get_jobs():
+@app.route('/jobkorea_api/jobs', methods=['GET'])
+def get_jobkrea_jobs():
     keyword = request.args.get('keyword', '데이터 분석')
     jobs = crawl_job_data(keyword)
     return jsonify(jobs)
 
+@app.route('/worknet_api/jobs', methods=['GET'])
+def get_worknet_jobs():
+    keyword = request.args.get('keyword', '데이터 분석')
+    jobs = crawl_job_data(keyword)
+    return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- crawler/jobkorea_crwaler.py
+++ crawler/jobkorea_crwaler.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.parse
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def crawl_job_data(keyword):
     encoded_keyword = urllib.parse.quote(keyword)
@@ -64,7 +64,7 @@ def crawl_job_data(keyword):
                 "info": job_info_list,
                 "details": job_details
             }
-            job_list.append(job_data)
+            job_list.append(job_data)#
         except Exception as e:
             print(f"Error processing job: {e}")
     return job_list
@@ -72,4 +72,77 @@ def crawl_job_data(keyword):
 keyword = '데이터 분석'
 job_data_list = crawl_job_data(keyword)
 json_result = json.dumps(job_data_list, ensure_ascii=False, indent=4)
-print(json_result)          
+print(json_result)
+
+
+
+# import requests
+# from bs4 import BeautifulSoup
+# import urllib.parse
+# import json
+# import time
+
+# def crawl_job_data(keyword):
+#     encoded_keyword = urllib.parse.quote(keyword)
+#     url = f"https://www.jobkorea.co.kr/Search/?stext={encoded_keyword}&tabType=recruit&Page_No=1"
+#     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     jobs = soup.select('article.list-item')
+#     job_list = []
+    
+#     for job in jobs[:20]:
+#         time.sleep(2)
+#         try:
+#             company_name = job.select_one('a.corp-name-link').text.strip()
+#             job_title_element = job.select_one('a.information-title-link.dev-view')
+
+#             if job_title_element:
+#                 job_title = job_title_element.text.strip()
+#                 link = urllib.parse.urljoin("https://www.jobkorea.co.kr", job_title_element['href'])
+#                 detail_url = urllib.parse.quote(link, safe=':/&=?')
+#             else:
+#                 job_title = "제목 없음"
+#                 detail_url = None
+
+#             job_info_list = []
+#             job_info_ul = job.select_one('ul.chip-information-group')
+
+#             if job_info_ul:
+#                 job_info_items = job_info_ul.find_all('li', class_='chip-information-item')
+#                 job_info_list = [item.text.strip() for item in job_info_items]
+
+#             salary = next((info for info in job_info_list if '만원' in info or '회사내규' in info), "협의 가능")
+#             career_level = next((info for info in job_info_list if '경력' in info or '신입' in info), "경력 무관")
+#             education_level = next((info for info in job_info_list if '학력' in info or '고졸' in info or '대졸' in info), "학력 무관")
+#             location = next((info for info in job_info_list if '서울' in info or '경기' in info or '부산' in info), "지역 미정")
+
+#             company_logo_url = "이미지 없음"
+#             if detail_url:
+#                 response_detail = requests.get(detail_url, headers={'User-Agent': 'Mozilla/5.0'})
+#                 soup_detail = BeautifulSoup(response_detail.text, "html.parser")
+
+#                 img_tag = soup_detail.find('img', id='cologo')
+#                 if img_tag and 'src' in img_tag.attrs:
+#                     company_logo_url = img_tag['src'].strip()
+
+#             job_data = {
+#                 "company_name": company_name,
+#                 "title": job_title,
+#                 "salary": salary,
+#                 "career_level": career_level,
+#                 "education_level": education_level,
+#                 "location": location,
+#                 "company_logo_url": company_logo_url,
+#                 "detail_url": detail_url
+#             }
+#             job_list.append(job_data)
+
+#         except Exception as e:
+#             print(f"Error processing job: {e}")
+
+#     return job_list
+
+# keyword = '데이터 분석'
+# job_data_list = crawl_job_data(keyword)
+# json_result = json.dumps(job_data_list, ensure_ascii=False, indent=4)
+# print(json_result)
